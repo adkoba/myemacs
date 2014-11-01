16442f4ea7d339b7bfcd25b0e06dc5589ef9b293
AdKoba <adkoba@gmail.com>
2014-11-01 16:54:44 +0100

    First init release with convenience setup file
    Signed-off-by: AdKoba <adkoba@gmail.com>

---
+++ init.el
@@ -0,0 +1,10 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; Add my config module path
+(add-to-list 'load-path "~/.emacs.d/custom/")
+
+;; Load my module
+(require 'setup-convenience)
